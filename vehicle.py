3bd652328652d8aa19e358ec02842153929d4f55
Fennec89 <fennecfox89@gmail.com>
2011-10-22 23:59:57 +0200

    Fixed the event handler, handles multiple key presses loplololol

--- vehicle.py
+++ vehicle.py
@@ -29,29 +29,29 @@ class Vehicle(entity.Entity):
 	def handle_input(self, event):
 		if event.type == KEYDOWN:
 			if event.key == K_UP:
-				self.y_vel = -5
+				self.y_vel -= 5
 			if event.key == K_DOWN:
-				self.y_vel = 5
+				self.y_vel += 5
 			if event.key == K_RIGHT:
-				self.x_vel = 5
+				self.x_vel += 5
 			if event.key == K_LEFT:
-				self.x_vel = -5
+				self.x_vel -= 5
 		elif event.type == KEYUP:
 			if event.key == K_UP:
-				self.y_vel = 0
+				self.y_vel += 5
 			if event.key == K_DOWN:
-				self.y_vel = 0
+				self.y_vel -= 5
 			if event.key == K_RIGHT:
-				self.x_vel = 0
+				self.x_vel -= 5
 			if event.key == K_LEFT:
-				self.x_vel = 0
+				self.x_vel += 5
 	
 	def update(self):
 		self.x_pos+=self.x_vel
 		self.y_pos+=self.y_vel
 	
 	def draw(self, screen):
-		pygame.draw.circle(screen, pygame.Color(255,0,0), (int(self.x_pos), int(self.y_pos)), 20)
+		pygame.draw.circle(screen, pygame.Color(125,10,88), (int(self.x_pos), int(self.y_pos)), 20)
 
 	def __repr__(self):
 		return "rot: %.2f, pos: (%.2f, %.2f)" % (self.rotation, self.x_pos, self.y_pos)
