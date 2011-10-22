105227f1bbab592b5370e4a1b58b5cad390a2fae
Fennec89 <fennecfox89@gmail.com>
2011-10-22 21:03:47 +0200

    Modfied to the better

--- vehicle.py
+++ vehicle.py
@@ -1,11 +1,43 @@
 #!/usr/bin/python2
+#coding=UTF-8
 #--------------------------------------
+#Name: vehicle.py
+#Class for the vehicles, handles 
+#input, update, draw...
 #
-#
+#Gustav Fahlén, 2011-10-22
 #-------------------------------------
 import pygame
+import entity
 
+class Vehicle(entity.Entity):
+	#def __init__(self):
+	#	self.x_pos=0.0
+	#	self.ypos=0.0
+	#	self.rotation=0
+	#	self.rotation_torque=0
 
+	def __init__(self, x_pos, y_pos, rotation):
+		self.x_pos=x_pos
+		self.y_pos=y_pos
+		self.rotation=rotation
+		self.rotation_torque=0
+	
+	def handle_input(self, event):
+		pass #so far, does nothing
+	
+	def update(self):
+		pass #so far, does nothing
+	
+	def draw(self, screen):
+		pass #so far, dows nothing
+
+	def __repr__(self):
+		return "rot: %.2f, pos: %.2f" % (self.x_pos, self.rotation)
+
+# debug main
 if __name__ == "__main__":
-	print "test main"
+	print "test_main"
+	V = Vehicle(0.0,0.0,90)
+	print V
 
