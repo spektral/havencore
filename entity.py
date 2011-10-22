222bc0954ff9f2f5ba2fc2e89086983f1796aed0
Christofer Odén <bei.oden@gmail.com>
2011-10-22 18:18:12 +0200

    Added the abstract base class "Entity"

---
+++ entity.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python2 -tt
+
+import pygame
+
+class Entity:
+	"""
+	Abstract base class.  All game objects should inherit from this.
+
+	"""
+	def handle_input(self, event):
+		raise NotImplementedError("Not implemented")
+
+	def update(self):
+		raise NotImplementedError("Not implemented")
+
+	def draw(self):
+		raise NotImplementedError("Not implemented")
+
+def main():
+	"""Unit test procedure"""
+
+if __name__ == "__main__":
+	main()
