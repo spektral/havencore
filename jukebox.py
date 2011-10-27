3bbbadab41a07bfad72bbdbd648190eeaa505072
Emil Österlund <emil@larsemil.se>
2011-10-27 19:34:23 +0200

    Fixed bug with muic in playlist not playing. Now it works. When last item in playlist stop playing it starts with the first. Next thing to do now is to implement different types of music, like battlemusic, soft music, townmusic etc. I have a version for that but it needs some more testing, and some naming of the songs in another way then current version. But it will come and it will be freakin awesome. Like awesomeness in square. Like awesome * awesome. Thats square. Yeah. And also i fixed naming for the variables. And that is also really really awesome. Not as awesome as the awesome in square, but quite awesome for being a naming standard. And now, i need to do something else.

--- jukebox.py
+++ jukebox.py
@@ -17,10 +17,10 @@ from pygame.locals import *
 
 class JukeBox:
 	def __init__(self):
-		self.soundPath = 'sounds/'
-		self.musicPath = 'music/'
+		self.sound_path = 'sounds/'
+		self.music_path = 'music/'
 
-		self.songs = glob.glob(self.musicPath+'*.ogg')
+		self.songs = glob.glob(self.music_path+'*.ogg')
 		self.sounds = {}
 		counter = 0
 		for track in self.songs:
@@ -30,23 +30,23 @@ class JukeBox:
 		self.play_song(self.track)
 
 	def play_song(self, track = 0):
-		print "Playing song: "+ str(track) +". "+ self.songs[track]
 		pygame.mixer.music.load(self.songs[track])
 		pygame.mixer.music.play()
 
 	def update(self):
-		if self.track +1 < len(self.songs): 
-			self.track = self.track +1
-		else:
-			self.track = 0
 
 		if pygame.mixer.music.get_busy():
 			return
 		else:
-			self.playSong(self.track)
+			if self.track +1 < len(self.songs): 
+				self.track = self.track +1
+			else:
+				self.track = 0
+	
+			self.play_song(self.track)
 
 	def load_sound(self, path, name):
-		self.sounds[name] = pygame.mixer.Sound(self.soundPath+path)
+		self.sounds[name] = pygame.mixer.Sound(self.sound_path+path)
 	
 	def play_sound(self, name):
 		self.sounds[name].play()
